0cdcdbc96eba01cf1b1cd2a23ce05358617a1ecb
gs <gs@aula11-13.almata.es>
2019-04-09 16:37:33 +0200

    e

--- js/constants.js
+++ js/constants.js
@@ -16,7 +16,10 @@ function myLoginFunction(){
 	/*
 	params api_key ( my api key)
 	cb the web that goes when user is authenticated relative path ( depends on the server is launched): http://localhost:3000/mainpage.ht*/
-	var url= 'http://www.last.fm/api/auth/?api_key='+clau_api+'&cb='+window.location.href +'main.html';
+	if(window.location.href.includes("index.html")){
+		var url=window.location.href.substring(0,window.location.href.length-10);
+	}
+	var url= 'http://www.last.fm/api/auth/?api_key='+clau_api+'&cb='+url+'main.html';
 	
 	window.location.replace(url);
 }
